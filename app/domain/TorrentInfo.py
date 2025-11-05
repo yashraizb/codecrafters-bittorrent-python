f80fb19c2aa1fdb6c2487881683f948126618997
Yash Rai <yashraizb@gmail.com>
2025-11-05 16:56:50 +0530

    codecrafters submit [skip ci]

--- app/domain/TorrentInfo.py
+++ app/domain/TorrentInfo.py
@@ -2,6 +2,8 @@ class TorrentInfo:
     def __init__(self, info):
         self.trackerURL = info.get(b'announce').decode()
         self.length = info.get(b'info').get(b'length')
+        self.infoHash = info.get(b'info')
+        print(self.infoHash)
     
     def printInfo(self):
         print("Tracker URL:", self.trackerURL)
