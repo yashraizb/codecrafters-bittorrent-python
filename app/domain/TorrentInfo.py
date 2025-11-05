9c320588bbd9937de3583d01d03943594276852f
Yash Rai <yashraizb@gmail.com>
2025-11-05 17:07:06 +0530

    codecrafters submit [skip ci]

--- app/domain/TorrentInfo.py
+++ app/domain/TorrentInfo.py
@@ -7,8 +7,12 @@ class TorrentInfo:
         self.trackerURL = info.get(b'announce').decode()
         self.length = info.get(b'info').get(b'length')
         self.infoHash = hashlib.sha1(bencodepy.encode(info.get(b'info'))).hexdigest()
+        self.pieceLength = info.get(b'info').get(b'piece length')
+        self.pieces = info.get(b'info').get(b'pieces')
     
     def printInfo(self):
         print("Tracker URL:", self.trackerURL)
         print("Length:", self.length)
         print("Info Hash:", self.infoHash)
+        print("Piece Length:", self.pieceLength)
+        print("Pieces:", self.pieces)
