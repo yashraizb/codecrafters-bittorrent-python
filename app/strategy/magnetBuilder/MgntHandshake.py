6f6cc7acb04c2b5915b6aeca85abd99907ce8137
Yash Rai <yashraizb@gmail.com>
2025-12-06 15:55:15 +0530

    codecrafters submit [skip ci]

--- app/strategy/magnetBuilder/MgntHandshake.py
+++ app/strategy/magnetBuilder/MgntHandshake.py
@@ -1,4 +1,5 @@
 import socket
+import bencodepy
 from app.strategy.torrentBuilder.OperationStrategy import OperationStrategy
 from app.builder.MagnetBuilder import MagnetBuilder
 from app.domain.HandshakeMessage import HandshakeMessage
@@ -22,6 +23,29 @@ class MgntHandshake(OperationStrategy):
 
         sock.send(handshakeMessageBytes)
         response = sock.recv(68)
+
+        # If the peer supports extension protocol, send extended handshake request
+        if response[20:28] == b'\x00\x00\x00\x00\x00\x10\x00\x00':
+            msg_len = sock.recv(4)
+            sock.recv(int.from_bytes(msg_len, byteorder="big"))
+
+            extension_len = sock.recv(4)
+            sock.recv(int.from_bytes(extension_len, byteorder="big"))
+
+            ben_dict = {
+                "m": {
+                    "ut_metadata": 18
+                }
+            }
+            
+            ben_dict_bytes = bencodepy.encode(ben_dict)
+            msg_id = (20).to_bytes(1, byteorder="big")
+            extension_id = (0).to_bytes(1, byteorder="big")
+            msg_len = (1 + 1 + len(ben_dict_bytes)).to_bytes(4, byteorder="big")
+            message = msg_len + msg_id + extension_id + ben_dict_bytes
+            sock.send(message)
+
+
         builder.handshakePeerId = response[48:].hex()
 
         # print(response)
